a28ec1fc29fbe104fbe2bfcb6cbcd48d56f38e68
ninja2 <hwinget@cleartechnologies.net>
2020-02-06 01:09:21 -0600

    initial commit

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // didn't quite work like I thought
+        // transform.position = new Vector3(transform.position.x, transform.position.y + _speed, 0);
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // public or private reference
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position and give it a new position (0, 0, 0) (x, y ,z)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
+
+    }
+
+    void CalculateMovement ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        // same as if y is greater that 0 or less than -3.8
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -11.2f, 11.2f), transform.position.x, 0);
+
+        if (transform.position.x >= 11.2f)
+        {
+            transform.position = new Vector3(-11.2f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.2)
+        {
+            transform.position = new Vector3(11.2f, transform.position.y, 0);
+        }
+    }
+}
